38da192c0fd9460cc83688b605cbb689f215f1fa
kenbio216 <3498592827@qq.com>
2024-05-13 11:06:39 +0800

    极限补上剩下的实验

---
+++ first_class_work/second_1.asm
@@ -0,0 +1,39 @@
+DATA SEGMENT
+    SCORE    DB 75            ; 定义一个字节的变量SCORE，假设考试分数为75
+    PASS_MSG DB 'PASS$'       ; 定义PASS消息
+    FAIL_MSG DB 'FAILURE$'    ; 定义FAILURE消息
+DATA ENDS
+
+CODE SEGMENT
+                   ASSUME CS:CODE, DS:DATA    ; 告诉汇编器代码段和数据段的寄存器
+
+    START:         
+    ; 初始化数据段
+                   MOV    AX, DATA
+                   MOV    DS, AX
+
+    ; 加载分数到寄存器AL
+                   MOV    AL, SCORE
+
+    ; 检查分数是否>=60
+                   CMP    AL, 60
+                   JGE    PASSCODE            ; 如果大于等于60, 跳转到PASSCODE
+
+    ; 如果分数小于60
+                   LEA    DX, FAIL_MSG        ; 加载FAILURE消息的地址到DX
+                   JMP    DISPLAY      ; 跳转到显示消息的代码部分
+
+    PASSCODE:      
+                   LEA    DX, PASS_MSG        ; 加载PASS消息的地址到DX
+
+    DISPLAY:
+    ; 使用DOS中断来显示消息
+                   MOV    AH, 09h
+                   INT    21h
+
+    ; 结束程序
+                   MOV    AH, 4Ch
+                   INT    21h
+
+CODE ENDS
+    END START
---
+++ first_class_work/second_3.asm
@@ -0,0 +1,54 @@
+DATA SEGMENT
+    score       DB  88          ; 定义一个字节的变量score，假设成绩为88
+    excellent   DB  'Excellent$'
+    medium      DB  'Medium level$'
+    pass        DB  'Pass$'
+    failure     DB  'Failure$'
+DATA ENDS
+
+CODE SEGMENT
+ASSUME CS:CODE, DS:DATA   ; 告诉汇编器代码段和数据段的寄存器
+
+START:
+    ; 初始化数据段
+    MOV AX, DATA
+    MOV DS, AX
+
+    ; 加载成绩到寄存器AL
+    MOV AL, score
+
+    ; 检查是否优秀
+    CMP AL, 85
+    JAE IsExcellent
+    CMP AL, 70
+    JAE IsMedium
+    CMP AL, 60
+    JAE IsPass
+    JMP IsFailure
+
+IsExcellent:
+    LEA DX, excellent
+    JMP DisplayMessage
+
+IsMedium:
+    LEA DX, medium
+    JMP DisplayMessage
+
+IsPass:
+    LEA DX, pass
+    JMP DisplayMessage
+
+IsFailure:
+    LEA DX, failure
+
+DisplayMessage:
+    ; 使用DOS中断来显示消息
+    MOV AH, 09h
+    INT 21h
+
+    ; 结束程序
+    MOV AH, 4Ch
+    INT 21h
+
+CODE ENDS
+    END START
